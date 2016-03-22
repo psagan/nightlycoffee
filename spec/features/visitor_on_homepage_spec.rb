c768b359c3b98b3644d3e99ac1c2ee288996cbc3
psagan <patryk.sagan@icloud.com>
2016-03-22 15:33:49 +0100

    #5 Initial Article model with first feature tests

--- spec/features/visitor_on_homepage_spec.rb
+++ spec/features/visitor_on_homepage_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 feature 'Visitor on homepage' do
   scenario 'sees articles header' do
-
     visit root_path
 
     expect(page).to have_css('h1'), text: 'Articles'
+  end
 
+  scenario 'sees list of latest articles' do
+    article = Article.create!(
+       title: 'First article',
+       content: 'Testing description'
+    )
+
+    visit root_path
+    expect(page).to have_content(article.title)
   end
 end
