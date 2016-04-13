804386462a8d5b1bfc3ffa2b1da35d08668591be
psagan <patryk.sagan@icloud.com>
2016-04-13 09:17:40 +0200

    Annotations for models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  content    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Article < ApplicationRecord
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  content    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Article, :type => :model do
