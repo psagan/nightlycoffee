3b45cf0ebe121034a11ca50ac23053961e946a4c
psagan <patryk.sagan@icloud.com>
2016-04-28 13:15:17 +0200

    Article Controller

--- app/assets/javascripts/backend/article/controllers/ArticleController.js
+++ app/assets/javascripts/backend/article/controllers/ArticleController.js
@@ -1,2 +1,6 @@
 angular.module('App')
-    .controller('ArticleCtrl', function () {});
+    .controller('ArticleCtrl', function ($resource) {
+
+        var Article = $resource('/articles/:articleId.json')
+        this.article = Article.get({articleId: 1})
+    });
