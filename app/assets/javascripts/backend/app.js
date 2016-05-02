e1444b2623b4e7a61ecc843fc79a0aa2a5e0a3a6
psagan <patryk.sagan@icloud.com>
2016-05-02 10:23:20 +0200

    Add article controller

--- app/assets/javascripts/backend/app.js
+++ app/assets/javascripts/backend/app.js
@@ -1,3 +1,14 @@
-angular.module('App', ['ngResource']);
+var app = angular.module('App', ['ngRoute', 'ngResource', 'templates'])
+
+    app.config(function($routeProvider) {
+
+        $routeProvider.when("/", {
+            controller: 'ArticleCtrl',
+            templateUrl: 'backend/article/tmpl/article_show.html',
+            controllerAs: 'ctrl'
+        })
+
+        .otherwise({redirectTo: '/'});
+    })
 
 
