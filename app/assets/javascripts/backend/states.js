b11cd1d5884dafceae88ee95ede782e0b642bcb8
psagan <patryk.sagan@icloud.com>
2016-05-12 08:20:17 +0200

    #6 Add js states for tags and modify for articles

--- app/assets/javascripts/backend/states.js
+++ app/assets/javascripts/backend/states.js
@@ -3,23 +3,43 @@ angular.module('App')
 
     $stateProvider.state('articles', {
         url: '/articles',
-        templateUrl: 'backend/article/tmpl/list.html',
+        templateUrl: 'backend/articles/tmpl/list.html',
         controller: 'ArticlesListController',
         controllerAs: 'ctrl'
     }).state('articlesShow', {
         url: '/articles/:id/show',
-        templateUrl: 'backend/article/tmpl/show.html',
+        templateUrl: 'backend/articles/tmpl/show.html',
         controller: 'ArticlesShowController',
         controllerAs: 'ctrl'
-    }).state('articleEdit', {
+    }).state('articlesEdit', {
         url: '/articles/:id/edit',
-        templateUrl: 'backend/article/tmpl/edit.html',
+        templateUrl: 'backend/articles/tmpl/edit.html',
         controller: 'ArticlesEditController',
         controllerAs: 'ctrl'
-    }).state('articleNew', {
+    }).state('articlesNew', {
         url: '/articles/new',
-        templateUrl: 'backend/article/tmpl/new.html',
+        templateUrl: 'backend/articles/tmpl/new.html',
         controller: 'ArticlesNewController',
+        controllerAs: 'ctrl'
+    }).state('tags', {
+        url: '/tags',
+        templateUrl: 'backend/tags/tmpl/list.html',
+        controller: 'TagsListController',
+        controllerAs: 'ctrl'
+    }).state('tagsShow', {
+        url: '/tags/:id/show',
+        templateUrl: 'backend/tags/tmpl/show.html',
+        controller: 'TagsShowController',
+        controllerAs: 'ctrl'
+    }).state('tagsEdit', {
+        url: '/tags/:id/edit',
+        templateUrl: 'backend/tags/tmpl/edit.html',
+        controller: 'TagsEditController',
+        controllerAs: 'ctrl'
+    }).state('tagsNew', {
+        url: '/tags/new',
+        templateUrl: 'backend/tags/tmpl/new.html',
+        controller: 'TagsNewController',
         controllerAs: 'ctrl'
     })
 })
