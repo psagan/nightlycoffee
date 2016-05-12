b4ef8dffba834c1bb84224e6fb56fca11527e896
psagan <patryk.sagan@icloud.com>
2016-05-12 10:11:26 +0200

    #6 Add tags resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/backend', to: 'backend#index', as: 'backend'
   resources :articles
+  resources :tags
   devise_for :users, controllers: { registrations: 'registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
