97d364f392e3b2dc61686c07292c55702f54432b
psagan <patryk.sagan@icloud.com>
2016-05-19 08:47:46 +0200

    #6 Tag with spec

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -12,27 +12,36 @@ class Tag < ApplicationRecord
   has_and_belongs_to_many :articles
 
   validates_presence_of :name
-  validates_length_of :name, minimum: 2
   validates_uniqueness_of :name, case_sensitive: false
 
-  before_validation :handle_name
+  before_validation :normalize
 
   class << self
+    # This class is responsible for handling all logic
+    # related to normalization - so external classes
+    # do not know about this process that name is normalized.
     def find_or_create_by(*args)
       find_params = args[0]
       if find_params[:name]
-        find_params[:name] = handle_name(find_params[:name])
+        find_params[:name] = normalize_name(find_params[:name])
       end
 
       super
     end
 
-    def handle_name(name)
+    # place it in one place so it can be used by instance and singleton's (class) methods
+    def normalize_name(name)
       name.downcase
     end
   end
 
-  def handle_name
-    self.name = self.class.handle_name(name)
+  private
+
+  def normalize
+    normalize_name
+  end
+
+  def normalize_name
+    self.name = self.class.normalize_name(name)
   end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -11,5 +11,53 @@
 require 'rails_helper'
 
 RSpec.describe Tag, :type => :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "::normalize_name" do
+    it "make name downcase" do
+      name = "PaTriCk"
+
+      result = Tag.normalize_name(name)
+
+      expect(result).to eq("patrick")
+    end
+  end
+
+  describe "::find_or_create_by" do
+    it "normalizes name" do
+      params = {name: "PaTriCk"}
+      allow(ApplicationRecord).to receive(:find_or_create_by)
+
+      Tag.find_or_create_by(params)
+
+      expect(Tag).to have_received(:normalize_name).with(params[:name]).once
+    end
+
+    it "calls super of itself" do
+      allow(ApplicationRecord).to receive(:find_or_create_by)
+      Tag.find_or_create_by({})
+
+      expect(ApplicationRecord).to have_received(:find_or_create_by).once
+    end
+
+    it "calls super with normalized name" do
+      params = {name: "PaTriCk"}
+      allow(ApplicationRecord).to receive(:find_or_create_by)
+
+      Tag.find_or_create_by(params)
+
+      expect(ApplicationRecord).to have_received(:find_or_create_by).with({name: "patrick"}).once
+    end
+  end
+
+  describe "#valid?" do
+    it "calls ::normalize_name" do
+      tag = Tag.new(name: "PatricK")
+      allow(Tag).to receive(:normalize_name)
+
+      tag.valid?
+
+      expect(Tag).to have_received(:normalize_name).with("PatricK").once
+    end
+  end
+
 end
